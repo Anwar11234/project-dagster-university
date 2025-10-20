173a0c8ad89e3249aec1077cb0f63eb1129a0cb3
Anwar11234 <87152151+Anwar11234@users.noreply.github.com>
2025-10-20 12:29:32 +0000

    mm

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -15,11 +15,15 @@ def taxi_trips_file() -> None:
     with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
         output_file.write(raw_trips.content)
 
+# src/dagster_essentials/defs/assets/trips.py
 @dg.asset
-def taxi_zones_file():
-  raw_zones = requests.get(
-    "https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
-  )
+def taxi_zones_file() -> None:
+    """
+      The raw CSV file for the taxi zones dataset. Sourced from the NYC Open Data portal.
+    """
+    raw_taxi_zones = requests.get(
+        "https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
+    )
 
-  with open(constants.TAXI_ZONES_FILE_PATH, 'wb') as output_file:
-    output_file.write(raw_zones.content)
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(raw_taxi_zones.content)
